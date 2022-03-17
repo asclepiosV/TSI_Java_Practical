0af7936164ec92b35c8b84ea5961e13dfa1b39ef
Paul Flammarion <pa.flammarion@gmail.com>
2022-03-17 11:10:24 +0200

    Ex2

---
+++ practical3/src/com/isep/Ex2/Main.java
@@ -0,0 +1,23 @@
+package com.isep.Ex2;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> listUpper = new ArrayList<>();
+        listUpper.add("a1");
+        listUpper.add("c3");
+        listUpper.add("a2");
+        listUpper.add("a3");
+        listUpper.add("b3");
+        listUpper.add("b2");
+        listUpper.add("c1");
+        listUpper.add("c2");
+        listUpper.add("b1");
+        for (int i = 0; i < listUpper.size(); i++) {
+            String newValue = listUpper.get(i).toUpperCase();
+            listUpper.set(i, newValue);
+        }
+        System.out.println(listUpper);
+    }
+}
