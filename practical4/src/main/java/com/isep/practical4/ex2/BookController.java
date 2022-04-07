3047619ec9d483cd1d3944a26f16a1eb1b54f916
Paul Flammarion <pa.flammarion@gmail.com>
2022-04-07 13:04:34 +0300

    p4 ex2 v2

--- practical4/src/main/java/com/isep/practical4/ex2/BookController.java
+++ practical4/src/main/java/com/isep/practical4/ex2/BookController.java
@@ -1,6 +1,8 @@
 package com.isep.practical4.ex2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +13,23 @@ import java.util.Optional;
 public class BookController {
     @Autowired
     private  BookRepository bookRepository;
-
-    @GetMapping(path = "/getBooks")
-    public Iterable<Book> getBooks(){
-        return bookRepository.findAll();
+    @GetMapping(path = "/*")
+    public ResponseEntity<String> error() {
+        return new ResponseEntity<>("<h1>Error 404</h1>" +
+                "<a href=\"/getBooks\">Get all books</a>"+
+                " or "+
+                "<a href=\"/getBook?id=1\">Get first book</a>", HttpStatus.NOT_FOUND);
     }
-
+    @GetMapping(path="/getBooks")
+    public ResponseEntity<Iterable<Book>> getBooks() {
+        return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
+    }
     @GetMapping("/getBook")
-    public Optional<Book> getBook(
+    public ResponseEntity<Optional<Book>> getBook(
             @RequestParam(
                     value = "id",
                     defaultValue = "1") Long id){
-        return bookRepository.findById(id);
+        return new ResponseEntity<>(bookRepository.findById(id), HttpStatus.OK);
     }
 
 }
